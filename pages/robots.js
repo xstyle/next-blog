8d873b0b299aa6ec4c3ce960f329947c9b0ebe59
Alex <unitxstyle@icloud.com>
2020-06-17 18:05:29 +0300

    Add card

--- pages/robots.js
+++ pages/robots.js
@@ -1,17 +1,31 @@
 import Head from 'next/head'
 import fetch from 'node-fetch'
 const BACKEND_URL = process.env.BACKEND
+
 export async function getServerSideProps(context) {
   const response = await fetch(BACKEND_URL + '/api/robot')
+  const options = {
+    headers: {
+      cookie: context.req.headers.cookie
+    }
+  }
+  const user_response = await fetch(BACKEND_URL + '/api/auth', options)
+
   let robots = []
+  let user = {}
 
   if (response.status === 200) {
     robots = await response.json()
   }
 
+  if (user_response.status === 200) {
+    user = await user_response.json()
+  }
+
   return {
     props: {
-      robots
+      robots,
+      user
     }
   }
 }
@@ -21,13 +35,26 @@ export default function Robots(props) {
     <Head>
       <title>Выберите робота</title>
     </Head>
+    <div>{props.user.name}</div>
     <h1>Выберите робота</h1>
-    <ul>
+    <div className="row">
       {
         props.robots.map(robot =>
-          <li key={robot._id}>{robot.name}</li>
+          <div className="col-12 col-lg-3" key={robot._id}>
+            <RobotCard robot={robot} />
+          </div>
         )
       }
-    </ul>
+    </div>
+  </div>
+}
+
+function RobotCard({ robot }) {
+  return <div className="card">
+    <div className="card-body text-center">
+      <h5 className="card-title">{robot.name}</h5>
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" className="btn btn-primary">Выбрать</a>
+    </div>
   </div>
 }
